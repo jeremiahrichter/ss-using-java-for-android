c98f370f2a38741d5a575a47cc457ebfe94cfb8f
jeremiah <jeremiahrichter@gmail.com>
2016-10-05 09:09:09 -0500

    add alphabetize()

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,22 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import static java.util.Collections.sort;
 
 public class Sorter {
-	private static ArrayList<String> userInput = new ArrayList<String>();
-	
-	private static ArrayList<String> reverse(ArrayList<String> array) {
-		ArrayList<String> reverse = new ArrayList<String>();
-		for (int i = array.size(); i >= 0; i--) {
-			reverse.add(array.get(i));
-		}
-		return reverse;
-	}
-	
-	private static ArrayList<String> alphabetize(ArrayList<String> array) {
-		return array;
-	}
-	
-	public static void main(String[] args) {
+    private static ArrayList<String> userInput = new ArrayList<String>();
 
-	}
+    private static ArrayList<String> reverse(ArrayList<String> array) {
+        ArrayList<String> reverse = new ArrayList<String>();
+        for (int i = array.size(); i >= 0; i--) {
+            reverse.add(array.get(i));
+        }
+        return reverse;
+    }
+
+    private static void alphabetize(ArrayList<String> array) {
+        sort(array);
+        userInput = array;
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
