c6e3ad5924ee343fe9fc0396f36d26e216c8ec41
jeremiah <jeremiahrichter@gmail.com>
2016-10-05 21:25:59 -0500

    fill out main method

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static java.util.Collections.sort;
 
@@ -15,7 +16,7 @@ public class Sorter {
 
     private static ArrayList<String> alphabetize(ArrayList<String> array) {
         sort(array);
-         return array;
+        return array;
     }
 
     private static void print(ArrayList<String> array) {
@@ -23,6 +24,41 @@ public class Sorter {
     }
 
     public static void main(String[] args) {
+        System.out.println("Type your inputs, enter \"-h\" for help: ");
+        Scanner in = new Scanner(System.in);
+        String input = "", word = "";
+        boolean r = false, a = false; //reverse, alphabetize flags
+        while (in.hasNextLine()) {
+            input = in.nextLine();
+            if (input.contains("-r")) {
+                r = true;
+                break;
+            } else if (input.contains("-a")) {
+                a = true;
+                break;
+            } else if (input.contains("-q")) {
+                break;
+            } else if (input.contains("-h")) {
+                System.out.println("Flags are:\n" +
+                        "-h for this help text\n" +
+                        "-r to reverse the input\n" +
+                        "-a to alphabetize the list of inputs entered\n" +
+                        "-q to quit");
+            } else {
+                userInput.add(input.toLowerCase());
+            }
+            in.close();
+
+            if (r) {
+                userInput = reverse(userInput);
+            } else if (a) {
+                userInput = alphabetize(userInput);
+            }
+            print(userInput);
+            System.out.println("--------------\n");
+            System.out.println("Thanks!");
+        }
+
     }
 
 }
